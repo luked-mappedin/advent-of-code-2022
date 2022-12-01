3b404e56cbcb248956165c9b4c3db3796fca4ee4
Luke Davis <luke.davis@mappedin.ca>
2022-12-01 11:05:41 -0500

    day1part2 complete

---
+++ day1/part2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+let inputArray = fs.readFileSync('./input', 'utf-8').split('\n').map(str => {
+    return(Number(str))
+});
+
+let finalCounts = [];
+
+let tempBuffer = 0
+// console.log(inputArray)
+
+for(x = 0; x < inputArray.length;x++){
+    if (inputArray[x] != 0){
+        tempBuffer += inputArray[x];
+    } else {
+        finalCounts.push(tempBuffer);
+        tempBuffer = 0;
+    }
+}
+
+let sortedArray = finalCounts.sort(function(a, b){return b - a})
+
+console.log(sortedArray[0] + sortedArray[1] + sortedArray[2]);
